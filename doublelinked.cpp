306e5e1e01bab1879981aba89811893e4265683a
SASCHADDD <saschadanu23@gmail.com>
2024-05-04 10:49:31 +0700

    STEP 1

--- doublelinked.cpp
+++ doublelinked.cpp
@@ -17,5 +17,8 @@ void addNode()
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode->noMhs; // Assign value too the data field of the new node
     cout << "\nEnter the name of the student: ";
-    cin >> newNode->name; //Assign value to the data field of the 
+    cin >> newNode->name; //Assign value to the data field of the new node
+
+    
+    
 }
