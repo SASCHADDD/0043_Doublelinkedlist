e470a99ba486c55ad9c588c7b0735bb9a3dad863
SASCHADDD <saschadanu23@gmail.com>
2024-05-04 21:05:22 +0700

    node to be deleted is not the first node

--- doublelinked.cpp
+++ doublelinked.cpp
@@ -96,4 +96,28 @@ void deleteNode()
     
     current = START; // step1: start from the first node
     previous = NULL;
+
+    // Locate the node to be deleted
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
+     if (current == NULL)
+     {
+        cout << "\033[31The record with roll number " << rollNo << " not found\033[0m" << endl;
+        return;
+     }
+     
+    // Node to be deleted is the first node
+    if (current == START)
+    {
+        START = START->next; // step 2: update the START pointer
+        if (START != NULL)
+        {
+            START->prev = NULL; 
+        }
+        
+    }
+    
 }
