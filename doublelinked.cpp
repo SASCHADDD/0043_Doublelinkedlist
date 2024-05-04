654ca849decaa22f8343735559a1e8f5196efd0e
SASCHADDD <saschadanu23@gmail.com>
2024-05-04 10:44:00 +0700

    commit 1

--- doublelinked.cpp
+++ doublelinked.cpp
@@ -13,5 +13,7 @@ void addNode()
 {
     Node *newNode = new Node(); //step 1; create a new node
     cout << "\nEnter the roll number of the student: ";
-    cin >> newNode->noMhs; /
+    cin >> newNode->noMhs; // Assign value too the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name; //Assign value to the data field of the 
 }
